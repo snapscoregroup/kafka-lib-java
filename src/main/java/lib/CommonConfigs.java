46f5ef02c9c354e6ad5a27d88b6fbcdd0a57bee0
mareksnap <marek@snapscore.com>
2022-11-07 10:37:46 +0100

    Updated ProducerConfigurationProperties

--- src/main/java/lib/CommonConfigs.java
+++ src/main/java/lib/CommonConfigs.java
@@ -3,8 +3,14 @@ package lib;
 public class CommonConfigs {
     public static final String BOOTSTRAP_SERVERS_CONFIG = "bootstrap.servers";
     public static final String GROUP_ID_CONFIG = "group.id";
+
     public static final String KEY_DESERIALIZER_CLASS_CONFIG = "key.deserializer";
     public static final String VALUE_DESERIALIZER_CLASS_CONFIG = "value.deserializer";
+    
+    public static final String KEY_SERIALIZER_CLASS_CONFIG = "key.serializer";
+    public static final String VALUE_SERIALIZER_CLASS_CONFIG = "value.serializer";
+
+
     public static final String REQUEST_TIMEOUT_MS_CONFIG = "request.timeout.ms";
     public static final String SECURITY_PROTOCOL = "security.protocol";
     public static final String SASL_MECHANISM = "sasl.mechanism";
--- src/main/java/lib/ProducerConfigurationProperties.java
+++ src/main/java/lib/ProducerConfigurationProperties.java
@@ -3,8 +3,13 @@ package lib;
 public class ProducerConfigurationProperties {
     public static final String BOOTSTRAP_SERVERS_CONFIG = CommonConfigs.BOOTSTRAP_SERVERS_CONFIG;
     public static final String GROUP_ID_CONFIG = CommonConfigs.GROUP_ID_CONFIG;
+
     public static final String KEY_DESERIALIZER_CLASS_CONFIG = CommonConfigs.KEY_DESERIALIZER_CLASS_CONFIG;
     public static final String VALUE_DESERIALIZER_CLASS_CONFIG = CommonConfigs.VALUE_DESERIALIZER_CLASS_CONFIG;
+
+    public static final String KEY_SERIALIZER_CLASS_CONFIG = CommonConfigs.KEY_SERIALIZER_CLASS_CONFIG;
+    public static final String VALUE_SERIALIZER_CLASS_CONFIG = CommonConfigs.VALUE_SERIALIZER_CLASS_CONFIG;
+
     public static final String AUTO_OFFSET_RESET_CONFIG = "auto.offset.reset";
     public static final String REQUEST_TIMEOUT_MS_CONFIG = CommonConfigs.REQUEST_TIMEOUT_MS_CONFIG;
     public static final String SECURITY_PROTOCOL = CommonConfigs.SECURITY_PROTOCOL;
