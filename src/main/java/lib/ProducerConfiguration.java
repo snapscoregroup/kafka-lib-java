baa65e847c73f70c3f762325c9fafc1b4889a033
mareksnap <marek@snapscore.com>
2022-11-07 07:15:03 +0100

    Add bootstrap servers producer configuration

--- src/main/java/lib/ProducerConfiguration.java
+++ src/main/java/lib/ProducerConfiguration.java
@@ -1,16 +1,11 @@
 package lib;
 
-import java.util.List;
 import java.util.Map;
 
-/**
- * @param bootstrapServers server ip address with port like 127.0.0.1:9092
- */
-
-public final record ProducerConfiguration(Map<String, Object> config, List<String> bootstrapServers) {
+public final record ProducerConfiguration(Map<String, Object> config) {
 
     public ProducerConfiguration {
-        if (bootstrapServers == null || bootstrapServers.isEmpty())
-            throw new IllegalArgumentException("The bootstrapServers is null or empty");
+        if (config == null || config.isEmpty())
+            throw new IllegalArgumentException("The producer config is null or empty");
     }
 }
