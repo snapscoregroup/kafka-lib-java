4b583a0de71c3cba0c15bc25b0fa1e0bfc1ed474
mareksnap <marek@snapscore.com>
2022-11-11 10:42:03 +0100

    Disabled logging

--- src/main/java/lib/KafkaProducerReact.java
+++ src/main/java/lib/KafkaProducerReact.java
@@ -1,7 +1,5 @@
 package lib;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
@@ -10,7 +8,7 @@ import reactor.kafka.sender.SenderRecord;
 import java.util.stream.Stream;
 
 public final class KafkaProducerReact<K, V> implements Producer<K, V> {
-    public static final Logger log = LoggerFactory.getLogger(KafkaProducerReact.class);
+//    public static final Logger log = LoggerFactory.getLogger(KafkaProducerReact.class);
 
     private final KafkaSender<K, V> sender;
 
@@ -30,7 +28,7 @@ public final class KafkaProducerReact<K, V> implements Producer<K, V> {
 
         sender.send(tFlux)
                 .doOnError(Throwable::printStackTrace)
-                .doOnNext(r -> log.info("Message send response: topic {}, offset {}", r.recordMetadata().topic(), r.recordMetadata().offset()))
+//                .doOnNext(r -> log.info("Message send response: topic {}, offset {}", r.recordMetadata().topic(), r.recordMetadata().offset()))
                 .subscribe();
     }
 
@@ -41,7 +39,7 @@ public final class KafkaProducerReact<K, V> implements Producer<K, V> {
 
         sender.send(senderRecordFlux)
                 .doOnError(Throwable::printStackTrace)
-                .doOnNext(r -> log.info("Message send response: topic {}, offset {}", r.recordMetadata().topic(), r.recordMetadata().offset()))
+//                .doOnNext(r -> log.info("Message send response: topic {}, offset {}", r.recordMetadata().topic(), r.recordMetadata().offset()))
                 .subscribe();
     }
 
