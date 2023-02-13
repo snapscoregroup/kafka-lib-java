d8bceae486c53cfb196487985c28b3b51f42faf6
mareksnap <marek@snapscore.com>
2023-02-13 09:49:32 +0100

    Avoid COMPRESSION_TYPE_CONFIG and COMPRESSION_GZIP

--- src/main/java/lib/CommonConfigs.java
+++ src/main/java/lib/CommonConfigs.java
@@ -3,19 +3,15 @@ package lib;
 public class CommonConfigs {
     public static final String BOOTSTRAP_SERVERS_CONFIG = "bootstrap.servers";
     public static final String GROUP_ID_CONFIG = "group.id";
-
     public static final String KEY_DESERIALIZER_CLASS_CONFIG = "key.deserializer";
     public static final String VALUE_DESERIALIZER_CLASS_CONFIG = "value.deserializer";
-
     public static final String KEY_SERIALIZER_CLASS_CONFIG = "key.serializer";
     public static final String VALUE_SERIALIZER_CLASS_CONFIG = "value.serializer";
-
-
     public static final String REQUEST_TIMEOUT_MS_CONFIG = "request.timeout.ms";
     public static final String SECURITY_PROTOCOL = "security.protocol";
     public static final String SASL_MECHANISM = "sasl.mechanism";
-
     public static final String MAX_REQUEST_SIZE_CONFIG = "max.request.size";
-
     public static final String BUFFER_MEMORY_CONFIG = "buffer.memory";
+    public static final String COMPRESSION_TYPE_CONFIG = "compression.type";
+    public static final String COMPRESSION_GZIP = "gzip";
 }
